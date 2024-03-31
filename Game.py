e626ff4a1c63db058248b77419f641d4db00c2c5
SUNDREAMcb <159593891+SUNDREAMcb@users.noreply.github.com>
2024-03-31 15:02:24 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -18,21 +18,23 @@ the difference between the terminal and the command window...?
 3.13 - Class for players. able to give each specific difficulty lives counter
 3.14 - Win Streak is not increasing beyond 1.
 3.15 - Player class should handle win streak, current difficulty, name.
-Need to simplify some things and be sure how each object should be called.
-3.16 - afraid i wouldn't be able to get back to where i was -  I solved most of the problems I was shooting for. 
-Class handles player lives, difficulty(indirect) and tracks win streak.
+I feel stumped. Need to simplify some things and be sure i know how i want each object to be called.
+3.16 - afraid i wouldn't be able to get back to where i was -  I solved most of the
+problems I was shooting for. Class handles player lives, difficulty(indirect) and tracks win streak.
 3.17 - I didnt have any ideas.
 3.18 - I want to add an input so user can quit altogether, choose a different difficulty "Quit" should quit the game,
 "Choose Difficulty" should exit current loop and go to former loop. Right now we have each Part of the game, but how we
 make it a functioning system is to build out the body.
-3.24 - Alright, i improved it and then broke it. lives  pull from a dict of keys, using that key list to display difficulty.
+3.24 -
+Alright, i improved it and then broke it. lives  pull from a dict of keys, using that
+key list to display difficulty. I think the getword is also broken as a result. Something in the create player function.
 3.27 - exceptionhandling. Game functions again! I'm able to focus on working on details. Now i want to select a difficulty mid game.
 """
 import random
 
 
 class Player:
-    playing = True #
+    playing = True
     difficulty_dict = {'test': 100, 'easy': 20, 'normal': 10, 'hard': 5}
     difficulty_list = list(difficulty_dict.keys())
 
@@ -47,7 +49,7 @@ username = input("What is your name?: ")
 
 def set_difficulty():
     """set amt of lives, which determines what word list to pull from  """
-    print(Player.difficulty_list)
+    print(Player.difficulty_list[::])
     choice = input("Which difficulty do you choose? ").lower()
     try:
         if choice.lower() in Player.difficulty_list:
@@ -61,7 +63,7 @@ def set_difficulty():
         print(err)
 
 
-user = Player(set_difficulty)
+user = Player(set_difficulty())
 
 
 def getword():
@@ -91,7 +93,7 @@ def getword():
                  "love", "happy", "sad", "funny", "fast", "slow", "big", "small", "hot", "cold"
                  ]
     test_list = ["test", "list"]
-    while Player.playing:
+    while user.playing:
         if user.lives == 100:
             word = random.choice(test_list)
             return word
@@ -104,14 +106,11 @@ def getword():
         if user.lives == 5:
             word = random.choice(hard_list)
             return word
-        else:
-            print("User is not playing anymore. ") #this shouldn't print? JIC IG..
 
 
 def hangman():  # main loop,
     """We are choosing to play the game"""
     hangman_word = getword()
-
     max_attempts = user.lives
     incorrect_guesses = 0
     guessed_letters = []
@@ -127,12 +126,12 @@ def hangman():  # main loop,
                         display_word[index] = guess
                         print("Good Job!")
                         print(" ".join(display_word))
-            elif guess.isalpha() and guess == 'quit':
-                print("Oh, you want to Quit. Okay. ")
-                break
             else:
                 incorrect_guesses += 1
                 print(f"Incorrect. You've made {incorrect_guesses} out of {max_attempts} remaining attempts. ")
+        elif guess.isalpha() and guess == 'quit':
+            print("Oh, you want to Quit. Okay. ")
+            break
         else:
             print("invalid input, please try again.")
     if '_' not in display_word:
@@ -145,8 +144,6 @@ def hangman():  # main loop,
         else:
             Player.playing = False
             return
-
-
     else:
         print("GAME OVER!")
         losersays = input("New Game?: ").lower
